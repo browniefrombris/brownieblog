83f549110479f2d78affc84f2120c38a59980051
Alexander Brown <abrownj.7@gmail.com>
2021-06-09 11:12:12 +1000

    favicon

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from flask import render_template, send_from_directory
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'username': 'Alexander'}
+	return render_template('index.html', title='Home', user=user)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static', 'images'), 'favicon.ico', mimetype = 'image/png')
+
+

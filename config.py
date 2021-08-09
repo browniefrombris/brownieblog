68dc328f5011c7b0f7bac6625da18c4454bd6e96
Alexander Brown <abrownj.7@gmail.com>
2021-08-09 11:35:14 +1000

    with postgresql

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or 'sqlite:///'+os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
